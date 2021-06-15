4b6342ecf71dcac0b81260a918906434482d39ce
bitflower <matthias.max@bitflower.net>
2021-06-15 20:42:35 +0200

    .

--- src/components/cv-page/cv-page.tsx
+++ src/components/cv-page/cv-page.tsx
@@ -10,28 +10,32 @@ import { TimeLineItem } from '../time-line/time-line-item.interface';
 
 const items: TimeLineItem[] = [
   {
-    label: '1998 - 2001',
+    label: '1999 - 2001',
     content: 'Schüler, Softwareentwickler',
   },
   {
-    label: '2001',
-    content: 'Abitur',
+    label: '2000 – 2006',
+    content: 'EDM-Artist & Musikproduzent - Mind One',
   },
   {
-    label: '2000 – 2006',
-    content: 'EDM-Artist & Musikproduzent',
+    label: '2003 - 2006',
+    content: 'Musikverleger & Plattenlabel - Breeze Records',
   },
   {
     label: '2002 - 2006',
-    content: 'Studium',
+    content: 'Studium Wirtschaftsinformatik - Fachhochschule Pforzheim',
   },
   {
     label: '2006',
     content: 'Diplom Wirtschaftsinformatik (FH)',
   },
   {
+    label: '2006 - 2010',
+    content: 'Freelance IT-Consultant - diverse Kunden',
+  },
+  {
     label: '2010 – 2014',
-    content: 'Songwriter und Autor',
+    content: 'Songwriter und Autor - BMG Rights Management Berlin',
   },
   {
     label: 'seit 2014',
