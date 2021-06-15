770e2bb24c3465861015ba596c358e13839b8653
bitflower <matthias.max@bitflower.net>
2021-06-15 09:42:22 +0200

    fix Safari?

--- src/components/project-list/project-list.tsx
+++ src/components/project-list/project-list.tsx
@@ -1,36 +1,30 @@
 import { Project, projects } from '@matthiasmax/cv-api';
-import { Component, h, Prop, State, Watch } from '@stencil/core';
+import { Build, Component, h, Prop, State, Watch } from '@stencil/core';
 
 import { compare } from '../../global/common';
 // import { ResponsiveContainer } from '@ionic-internal/ionic-ds';
+
+const sort = (tobesorted: Project[]) => {
+  return tobesorted.sort(compare('timestamp', -1));
+};
 
 @Component({
   tag: 'project-list',
   styleUrl: 'project-list.css',
 })
 export class ProjectList {
-  constructor() {
-    this.selected = this.sort(projects);
-  }
-
   @Prop()
   industry?: string;
   @Watch('industry')
   industryHandler() {
-    const filteredIndustries = !this.industry ? projects : projects.filter(project => project.industry === this.industry);
-    this.selected = this.sort(filteredIndustries);
-  }
-
-  private sort(tobesorted: Project[]) {
-    return tobesorted.sort(compare('timestamp', -1));
-  }
-
-  componentWillLoad() {
-    this.industryHandler();
+    if (!Build.isServer) {
+      const filteredIndustries = !this.industry ? projects : projects.filter(project => project.industry === this.industry);
+      this.selected = sort(filteredIndustries);
+    }
   }
 
   @State()
-  private selected: Project[] = [];
+  private selected: Project[] = sort(projects);
 
   render() {
     return this.selected.map(project => <project-card class="project-list__project" project={project} />);
