36d81b60b1c679896b36d388b57b00bcac6faec9
bitflower <matthias.max@bitflower.net>
2021-06-15 10:46:46 +0200

    1. iteration project-card

--- src/components/project-card/project-card.tsx
+++ src/components/project-card/project-card.tsx
@@ -20,37 +20,50 @@ export class ProjectCard {
   render() {
     return (
       <Fragment>
-        <h3 class="project-card__info-block-heading">
-          <span class="project-card__info-block-heading-title">{this.project.title}</span>
-          {this.project.deliveryTypes.map(item => (
-            <info-badge class="project-card__info-block-heading-delivarable">{item}</info-badge>
-          ))}
-        </h3>
-        {this.project.description ? (
-          <p class="project-card__info-block">
-            <span>{this.project.description}</span>
-          </p>
-        ) : null}
-        <p class="project-card__info-block">
-          <div class="label">Kunde:</div>
-          <span>{this.project.customer}</span>
-        </p>
-        <p class="project-card__info-block">
-          <div class="label">Branche:</div>
-          <span>{this.project.industry}</span>
-        </p>
-        <p class="project-card__info-block">
-          <div class="label">Zeitraum:</div>
-          <span>{this.project.year}</span>
-        </p>
-        <p class="project-card__info-block">
-          <div class="label">Tätigkeiten:</div>
-          <span>{this.project.taskTypes.sort(compare()).join(', ')}</span>
-        </p>
-        <p class="project-card__info-block">
-          <div class="label">Einsatz von:</div>
-          <span>{this.project.toolsUsed.sort(compare()).join(', ')}</span>
-        </p>
+        <div class="project-card__left">
+          <h3 class="project-card__info-block-heading">
+            <span class="project-card__info-block-heading-title">{this.project.title}</span>
+            {this.project.deliveryTypes.map(item => (
+              <info-badge class="project-card__info-block-heading-delivarable">{item}</info-badge>
+            ))}
+          </h3>
+          {this.project.description ? (
+            <div class="project-card__info-block">
+              <span>{this.project.description}</span>
+            </div>
+          ) : null}
+
+          <div class="project-card__info-block">
+            <h4 class="label">Tätigkeiten:</h4>
+            <span>{this.project.taskTypes.sort(compare()).join(', ')}</span>
+          </div>
+          <div class="project-card__info-block">
+            <h4 class="label">Einsatz von:</h4>
+            <span>{this.project.toolsUsed.sort(compare()).join(', ')}</span>
+          </div>
+        </div>
+        <div class="project-card__right">
+          <div class="kpis">
+            <div class="project-card__info-block">
+              <h4 class="label">Kunde</h4>
+              <h2 class="kpi">{this.project.customer}</h2>
+            </div>
+          </div>
+          <div class="kpis equal-kpis">
+            <div class="project-card__info-block">
+              <h4 class="label">Branche</h4>
+              <h2 class="kpi">{this.project.industry}</h2>
+            </div>
+            <div class="project-card__info-block">
+              <h4 class="label">Jahr</h4>
+              <h2 class="kpi">{this.project.year}</h2>
+            </div>
+            <div class="project-card__info-block">
+              <h4 class="label">Dauer</h4>
+              <h2 class="kpi">6 Monate</h2>
+            </div>
+          </div>
+        </div>
       </Fragment>
     );
   }
