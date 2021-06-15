bb2ef30eb088d8ce1273060f8d55f29e0ee05c87
bitflower <matthias.max@bitflower.net>
2021-06-15 11:29:22 +0200

    huge style refactor

--- src/components/project-card/project-card.tsx
+++ src/components/project-card/project-card.tsx
@@ -21,46 +21,52 @@ export class ProjectCard {
     return (
       <Fragment>
         <div class="project-card__left">
-          <h3 class="project-card__info-block-heading">
-            <span class="project-card__info-block-heading-title">{this.project.title}</span>
-            {this.project.deliveryTypes.map(item => (
-              <info-badge class="project-card__info-block-heading-delivarable">{item}</info-badge>
-            ))}
-          </h3>
-          {this.project.description ? (
-            <div class="project-card__info-block">
-              <span>{this.project.description}</span>
-            </div>
-          ) : null}
+          <div class="dots-block dots"></div>
+          <div class="content-card">
+            <h3 class="project-card__info-block-heading-title">{this.project.title}</h3>
+            {this.project.description ? (
+              <div class="project-card__info-block">
+                <p>{this.project.description}</p>
+              </div>
+            ) : null}
 
-          <div class="project-card__info-block">
-            <h4 class="label">Tätigkeiten:</h4>
-            <span>{this.project.taskTypes.sort(compare()).join(', ')}</span>
-          </div>
-          <div class="project-card__info-block">
-            <h4 class="label">Einsatz von:</h4>
-            <span>{this.project.toolsUsed.sort(compare()).join(', ')}</span>
+            <div class="project-card__info-block">
+              <h4 class="label">Tätigkeiten:</h4>
+              <p>{this.project.taskTypes.sort(compare()).join(', ')}</p>
+            </div>
+            <div class="project-card__info-block">
+              <h4 class="label">Einsatz von:</h4>
+              <p>{this.project.toolsUsed.sort(compare()).join(', ')}</p>
+            </div>
           </div>
         </div>
-        <div class="project-card__right">
-          <div class="kpis">
+        <div class="project-card__right kpi-rows">
+          <div class="kpis equal-kpis">
             <div class="project-card__info-block">
               <h4 class="label">Kunde</h4>
-              <h2 class="kpi">{this.project.customer}</h2>
+              <h2 class="kpi small-kpi">{this.project.customer}</h2>
+            </div>
+            <div class="project-card__info-block">
+              <h4 class="label">&nbsp;</h4>
+              <h2 class="kpi small-kpi">
+                {this.project.deliveryTypes.map(item => (
+                  <info-badge class="project-card__info-block-heading-delivarable">{item}</info-badge>
+                ))}
+              </h2>
             </div>
           </div>
           <div class="kpis equal-kpis">
             <div class="project-card__info-block">
               <h4 class="label">Branche</h4>
-              <h2 class="kpi">{this.project.industry}</h2>
+              <h2 class="kpi large-kpi">{this.project.industry}</h2>
             </div>
             <div class="project-card__info-block">
               <h4 class="label">Jahr</h4>
-              <h2 class="kpi">{this.project.year}</h2>
+              <h2 class="kpi large-kpi">{this.project.year}</h2>
             </div>
             <div class="project-card__info-block">
               <h4 class="label">Dauer</h4>
-              <h2 class="kpi">6 Monate</h2>
+              <h2 class="kpi large-kpi">6 Monate</h2>
             </div>
           </div>
         </div>
