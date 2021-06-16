402db8ccbe5e7cf0b8bf392951487fa90ca826cb
bitflower <matthias.max@bitflower.net>
2021-06-16 12:11:06 +0200

    .

--- src/components/cv-page/cv-page.tsx
+++ src/components/cv-page/cv-page.tsx
@@ -2,13 +2,15 @@ import { Component, Fragment, h, State } from '@stencil/core';
 import { ResponsiveContainer } from '@ionic-internal/ionic-ds';
 import { milestones } from '@matthiasmax/cv-api';
 
+import i18n from '../../stores/i18n.store';
+
 @Component({
   tag: 'cv-page',
   styleUrl: 'cv-page.css',
 })
 export class cvPage {
   constructor() {
-    document.title = `CV of Matthias Max`;
+    document.title = i18n.cv.title;
   }
 
   @State()
@@ -29,22 +31,22 @@ export class cvPage {
         <header class="hide-screen">
           <div class="bla">
             <app-icon name="logo" />
-            <h1 class="cv-page__kopfzeiletitle">Web Technologie Consultant und Fullstack Developer</h1>
+            <h1 class="cv-page__kopfzeiletitle">{i18n.cv.heading}</h1>
           </div>
         </header>
         <ResponsiveContainer>
           <div class="page new-page">
             <div class="left">
-              <h1 class="hide-print">Web Technologie Consultant und Fullstack Developer</h1>
-              <h2 class="cv-page__first-heading">Professionelle Laufbahn</h2>
+              <h1 class="hide-print">{i18n.cv.heading}</h1>
+              <h2 class="cv-page__first-heading">{i18n.cv.cv}</h2>
               <time-line items={milestones.reverse()} />
 
-              <h2>Ausbildung</h2>
+              <h2>{i18n.cv.education}</h2>
               <p>
                 Studium der Wirtschaftsinformatik - Fachhochschule Pforzheim, 2001 – 2006 <br />
                 Thema der Diplomarbeit: TITEL DIPLOMARBEIT
               </p>
-              <h2>Fremdsprachen</h2>
+              <h2>{i18n.cv.languages}</h2>
               <p>
                 Englisch: fließend in Wort und Schrift <br />
                 Französisch: Schulkenntnisse
@@ -59,11 +61,15 @@ export class cvPage {
             </div>
           </div>
           <div class="new-page">
-            <h2>Projekterfahrung</h2>
+            <h2>{i18n.cv.experience}</h2>
             <div class="filters hide-print">
-              <h4>Branchen: {this.selectedIndustry || 'Alle'}</h4>
+              <h4>
+                {i18n.industry.plural}: {this.selectedIndustry || i18n.industries.all}
+              </h4>
               <project-industry-filter onMmChange={this.onIndustryChange} />
-              <h4>Deliverables: {this.selectedDeliverable || 'Alle'}</h4>
+              <h4>
+                {i18n.deliverable.singular}: {this.selectedDeliverable || i18n.deliverables.all}
+              </h4>
               <project-deliverables-filter onMmChange={this.onDeliverableChange} />
             </div>
             <project-list industry={this.selectedIndustry} deliverable={this.selectedDeliverable} />
--- src/stores/i18n.interface.ts
+++ src/stores/i18n.interface.ts
@@ -1,4 +1,23 @@
+interface I18nCV {
+  cv: string;
+  education: string;
+  experience: string;
+  heading: string;
+  languages: string;
+  title: string;
+}
 interface I18nDeliverables {
+  all: string;
+  app: string;
+  content: string;
+  desktop: string;
+  library: string;
+  masterRecording: string;
+  pwa: string;
+  spa: string;
+  website: string;
+}
+interface I18nIndustries {
   all: string;
   app: string;
   content: string;
@@ -12,7 +31,17 @@ interface I18nDeliverables {
 
 export interface I18n {
   lang: Languages;
+  cv: I18nCV;
+  deliverable: {
+    singular: string;
+    plural: string;
+  };
   deliverables: I18nDeliverables;
+  industry: {
+    singular: string;
+    plural: string;
+  };
+  industries: I18nIndustries;
 }
 
 export type Languages = 'en' | 'de';
