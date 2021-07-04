205d0e623b3e49c2f2b4af2f24c374a59bed9c53
bitflower <matthias.max@bitflower.net>
2021-07-04 20:53:10 +0200

    .

--- src/components/blog-page/blog-page.tsx
+++ src/components/blog-page/blog-page.tsx
@@ -30,8 +30,7 @@ export class BlogPage {
           <h1 class="hero-heading">Let's get better together.</h1>
           <h2 class="no-print-font hero-subheading">I've learned a lot from the community. This blog is where I give back.</h2>
           <p class="intro-paragraph no-print-font">
-            &bdquo;Sharing is caring&rdquo; &#8212; they say. And I couldn't agree more. From time to time I share my knowledge, experiences and thoughts on topics like business,
-            technology and design. Feel free to&nbsp;
+            From time to time I share my knowledge, experiences and thoughts on topics like business, technology and design. Feel free to&nbsp;
             <stencil-route-link url={i18n.contact.link}>get in touch for feedback or to raise questions.</stencil-route-link>
           </p>
           <blog-list />
